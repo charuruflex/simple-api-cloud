c6c8435c0ae5dc5a42d39f34c017d6213279252b
sharuru <charlyf65@gmail.com>
2018-12-03 17:04:53 +0100

    get correct var env for redis

--- main.go
+++ main.go
@@ -137,7 +137,7 @@ func loadConfig(file string) (cfg config, err error) {
 	cfg.Server.WriteTimeout = time.Second * 15
 	cfg.Server.ReadTimeout = time.Second * 15
 	cfg.Server.IdleTimeout = time.Second * 60
-	cfg.Database.Redis = os.Getenv("REDIS_MASTER_SERVICE_HOST")
+	cfg.Database.Redis = os.Getenv("REDIS_MASTER_PORT")
 
 	data, err := ioutil.ReadFile(file)
 	if err != nil {
